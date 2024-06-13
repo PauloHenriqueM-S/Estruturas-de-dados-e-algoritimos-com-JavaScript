75348f03caf2efb5d2d7d690b797cc2d910e3a3c
Paulo Henrique <phttm0501@gmail.com>
2024-06-13 18:21:54 -0400

    Next Functions

---
+++ Cap1/04-Operators.js
@@ -0,0 +1,28 @@
+var num = 0; // {1}
+num = num + 2;
+num = num * 3;
+num = num / 2;
+num++;
+num--;
+num += 1; // {2}
+num -= 2;
+num *= 3;
+num /= 2;
+num %= 3;
+console.log('num == 1 : ' + (num == 1)); // {3}
+console.log('num === 1 : ' + (num === 1));
+console.log('num != 1 : ' + (num != 1));
+console.log('num > 1 : ' + (num > 1));
+console.log('num < 1 : ' + (num < 1));
+console.log('num >= 1 : ' + (num >= 1));
+console.log('num <= 1 : ' + (num <= 1));
+console.log('true && false : ' + (true && false)); // {4}
+console.log('true || false : ' + (true || false));
+console.log('!true : ' + (!true));
+
+console.log('5 & 1:', (5 & 1));
+console.log('5 | 1:', (5 | 1));
+console.log('~ 5:', (~5));
+console.log('5 ^ 1:', (5 ^ 1));
+console.log('5 << 1:', (5 << 1));
+console.log('5 >> 1:', (5 >> 1));
